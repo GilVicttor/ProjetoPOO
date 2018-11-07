c99a5b5233a5c3d14d58c6da1d20bc7b7c96bd9f
GilVicttor <33091208+GilVicttor@users.noreply.github.com>
2018-11-07 01:10:35 -0200

    Create Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,31 @@
+public class Usuario {
+  private String apelido;
+  private String senha;
+
+ public Usuario(){
+   this.apelido = null;
+   this.senha = null;
+ } 
+ public Usuario(String nome, String senha){
+   
+ }
+ public String getApelido(){
+   return this.apelido;
+ }
+ public void setApelido(String apelido){
+   this.apelido = apelido;
+ }
+
+ public String getSenha(){
+   return this.senha;
+ }
+ public void setSenha(String senha){
+   this.senha = senha;
+  
+ }
+
+    @Override
+    public String toString() {
+        return "Usuário {" + "apelido:" + apelido + ", senha: " + senha + '}';
+    }
+ 
