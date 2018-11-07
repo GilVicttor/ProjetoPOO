fcd7504f049da298ebb8cffa667c93bf70adeb9e
GilVicttor <33091208+GilVicttor@users.noreply.github.com>
2018-11-07 01:09:19 -0200

    Create Turma.java

---
+++ Turma.java
@@ -0,0 +1,37 @@
+public class Turma {
+    
+    private int identificador;
+    private double ano_oferta;
+    
+    public Turma(){
+        this.identificador = 00;
+        this.ano_oferta = 0000;
+        
+    }
+    public Turma(int id, double anoOferta){
+        this.identificador = id;
+        this.ano_oferta = ano_oferta;
+    }
+
+    public int getIdentificador() {
+        return this.identificador;
+    }
+
+    public void setIdentificador(int identificador) {
+        this.identificador = identificador;
+    }
+
+    public double getAno_oferta() {
+        return this.ano_oferta;
+    }
+
+    public void setAno_oferta(double ano_oferta) {
+        this.ano_oferta = ano_oferta;
+    }
+
+    @Override
+    public String toString() {
+        return "Turma {" + "identificador:" + this.identificador + ", ano de oferta: " + this.ano_oferta + '}';
+    }
+    
+}
