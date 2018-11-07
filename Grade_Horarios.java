6f0bf5e30075afc9d416658c14551ac3f5adc059
GilVicttor <33091208+GilVicttor@users.noreply.github.com>
2018-11-07 01:00:27 -0200

    Create Grade_Horarios.java

---
+++ Grade_Horarios.java
@@ -0,0 +1,25 @@
+public class GradeDeHorarios {
+    int i;
+    int c;
+    Horario[][] horarios = new Horario [7][7];
+    
+    @Override
+    public String toString(){    
+        String [] dias = {"segunda ", "terça ", "quarta ", "quinta ", "sexta"} ; 
+        String dados = "";
+        for (i = 0; i <5 ; i++) {
+            dados+=dias[i] + "\n";
+            for(c = 0; c < 6 ; c++){
+                dados += horarios[i][c] +" ";
+            }
+            dados+="\n";
+        }
+        return dados;
+    }
+
+    public void addHorarios(int dia, Horario h){
+        this.horarios[h.getAula()][dia] = h;
+    };
+    
+    
+}
